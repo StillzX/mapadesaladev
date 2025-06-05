327bcdc84c566a1c092d7ea3d9b3c6571f9a526f
Miguel C. Gonçalves <studio.dev2909@gmail.com>
2025-06-04 23:22:10 -0400

    Novas funcionalidades adicionadas, correção de bugs, quase completo o projeto

--- asset/js/home_page.js
+++ asset/js/home_page.js
@@ -1,4 +1,7 @@
-function obterSaudacao (){
+let btnImportar = document.getElementById('btnImportar');
+let inputImportar = document.getElementById('inputImportar');
+
+function obterSaudacao() {
     const agora = new Date()
     const hora = agora.getHours();
 
@@ -13,45 +16,6 @@ function obterSaudacao (){
     }
 
     return saudacao;
-}
-
-function criarMapa () {
-    let anoturma = document.getElementById('anoturma').value;
-    let descricaoturma = document.getElementById('descricaoturma').value;
-    let text_anoturma;
-    let text_descricaoturma;
-
-    if (!anoturma) {
-        alert('Digite o ano da turma!');
-        return;
-    } else {
-        text_anoturma = anoturma;
-
-        if (descricaoturma) {
-            text_descricaoturma = descricaoturma;
-        }
-    }
-
-    const dadosMapa = {
-        nome: text_anoturma,
-        descricao: text_descricaoturma,
-        numero_alunos: 0,
-        alunos: {},
-        numero_carteiras: 0,
-        fileiras: 0
-    }
-
-    const jsonStr = JSON.stringify(dadosMapa, null, 2);
-
-      const blob = new Blob([jsonStr], { type: "application/json" });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = `${text_anoturma}.json`;
-      a.click();
-      URL.revokeObjectURL(url);
-
-    window.location.href = 'criar_mapa.html'
 }
 
 document.querySelector('#text-saudacao').textContent = obterSaudacao();
